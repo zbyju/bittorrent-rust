cab915f6168c29ae5966fbe55c43448b7294cc50
zbyju <zbynek.jurica@gmail.com>
2023-12-05 00:28:58 +0100

    Add decoding ints

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    if encoded_value.chars().next().unwrap().is_ascii_digit() {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
+        serde_json::Value::String(string.to_string())
+    } else if encoded_value.starts_with('i') {
+        let colon_index = encoded_value.find('e').unwrap();
+        let number_string = &encoded_value[1..colon_index];
+        let number = number_string.parse::<i64>().unwrap();
+        serde_json::Value::Number(serde_json::Number::from(number))
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -27,7 +32,7 @@ fn main() {
     if command == "decode" {
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        println!("{}", decoded_value);
     } else {
         println!("unknown command: {}", args[1])
     }
