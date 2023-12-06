f4c3a4f34a297d8f27dbfe539dc0d398079116ba
zbyju <zbynek.jurica@gmail.com>
2023-12-06 17:14:13 +0100

    Add decoding list

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serde_json;
+use serde_json::{self};
 use std::env;
 
 // Available if you need it!
@@ -6,22 +6,59 @@ use std::env;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_ascii_digit() {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        serde_json::Value::String(string.to_string())
-    } else if encoded_value.starts_with('i') {
-        let colon_index = encoded_value.find('e').unwrap();
-        let number_string = &encoded_value[1..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        serde_json::Value::Number(serde_json::Number::from(number))
+    decode(encoded_value, 0).unwrap().0
+}
+
+fn decode(encoded_value: &str, index: usize) -> Option<(serde_json::Value, usize)> {
+    let first_char = encoded_value.chars().nth(index).unwrap();
+    if first_char.is_ascii_digit() {
+        decode_string(encoded_value, index)
+    } else if first_char == 'i' {
+        decode_number(encoded_value, index)
+    } else if first_char == 'l' {
+        decode_list(encoded_value, index)
+    } else if first_char == 'd' {
+        decode_dict(encoded_value, index)
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
+}
+
+fn decode_string(encoded_value: &str, index: usize) -> Option<(serde_json::Value, usize)> {
+    let colon_index = encoded_value.get(index..)?.find(':')? + index;
+    let number_string = &encoded_value[index..colon_index];
+    let number = number_string.parse::<i64>().ok()? as usize;
+    Some((
+        serde_json::Value::String(
+            encoded_value[colon_index + 1..colon_index + 1 + number].to_string(),
+        ),
+        colon_index + number + 1,
+    ))
+}
+
+fn decode_number(encoded_value: &str, index: usize) -> Option<(serde_json::Value, usize)> {
+    let e_index = encoded_value.get(index..)?.find('e')? + index;
+    let number_string = &encoded_value[index + 1..e_index];
+    let number = number_string.parse::<i64>().ok()?;
+    Some((
+        serde_json::Value::Number(serde_json::Number::from(number)),
+        e_index + 1,
+    ))
+}
+
+fn decode_list(encoded_value: &str, index: usize) -> Option<(serde_json::Value, usize)> {
+    let mut res = Vec::new();
+    let mut i = index + 1;
+    while encoded_value.chars().nth(i)? != 'e' {
+        let decoded = decode(encoded_value, i)?;
+        res.push(decoded.0);
+        i = decoded.1;
+    }
+    Some((serde_json::Value::Array(res), i + 1))
+}
+
+fn decode_dict(encoded_value: &str, index: usize) -> Option<(serde_json::Value, usize)> {
+    Some((serde_json::Value::String("test".to_string()), 0))
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
